dde793cb880929177491237257c083f138ec2d12
QIEN LIN <qienlin@QIENdeMacBook-Pro.local>
2020-02-12 10:16:43 -0800

    deploy

---
+++ docs/precache-manifest.fbb38f1e1eeab5d046f4d7104ada3447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1731315a4a046ef0e44109c05f37b5e7",
+    "url": "/estherlin-098/portfolio/index.html"
+  },
+  {
+    "revision": "90a689a702920d8ad1f6",
+    "url": "/estherlin-098/portfolio/static/css/main.2bae37e3.chunk.css"
+  },
+  {
+    "revision": "3f7c1abb021fc6586ab9",
+    "url": "/estherlin-098/portfolio/static/js/2.9bc43008.chunk.js"
+  },
+  {
+    "revision": "90a689a702920d8ad1f6",
+    "url": "/estherlin-098/portfolio/static/js/main.be53f578.chunk.js"
+  },
+  {
+    "revision": "8f8ead484470704b3cbb",
+    "url": "/estherlin-098/portfolio/static/js/runtime~main.d9e3fd75.js"
+  },
+  {
+    "revision": "4317be933b8b615a4b7847d40093ae3f",
+    "url": "/estherlin-098/portfolio/static/media/header.4317be93.jpg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Header from './components/header';
+import About from './components/about';
+import Resume from './components/resume';
+import Portfolio from './components/portfolio';
+import Contact from './components/contact';
+import Footer from './components/footer';
+import './App.css'
+//
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//git clone (paste the git hpps link)
+//cd portfolio
+//npm install
+
+//kill the previous re link (rm -rf .git)
+//
